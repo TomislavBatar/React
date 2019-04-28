4da879dc07f3ee2df56015f7094228c2959d89dc
TomislavBatar <wmbl.info@gmail.com>
2019-04-28 14:12:55 +0200

    Updates

---
+++ precache-manifest.4a6b43bee5d774672d6c38a840af8d11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "26aa74fe6aa82b0e2d0aee8e59c92cb0",
+    "url": "/React/static/media/logo.26aa74fe.svg"
+  },
+  {
+    "revision": "2a2072bb025e727d58d1a1e85c3579f7",
+    "url": "/React/static/media/bgImage.2a2072bb.jpg"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "1271631202a1dcd938fd",
+    "url": "/React/static/js/main.a4d1fab9.chunk.js"
+  },
+  {
+    "revision": "d3825d681c3bc276674b",
+    "url": "/React/static/js/2.b1d68ba0.chunk.js"
+  },
+  {
+    "revision": "1271631202a1dcd938fd",
+    "url": "/React/static/css/main.4e3a9099.chunk.css"
+  },
+  {
+    "revision": "14fbdb0f1defb3b6665eddb7eb2f5abd",
+    "url": "/React/index.html"
+  }
+];
